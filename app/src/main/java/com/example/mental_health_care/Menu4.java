acbab452930c66da70e9f21af9ad157dc5340f83
Jungpyo Hong <jzh0169@auburn.edu>
2020-07-09 19:16:42 -0500

    add google map content to menu 4

--- app/src/main/java/com/example/mental_health_care/Menu4.java
+++ app/src/main/java/com/example/mental_health_care/Menu4.java
@@ -1,5 +1,39 @@
 package com.example.mental_health_care;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 //resource center about location of mental health care
-public class Menu4 {
+public class Menu4 extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu4);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapAPI);
+
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mapAPI = googleMap;
+        LatLng location1 = new LatLng(19,12);
+        mapAPI.addMarker(new MarkerOptions().position(location1).title("test1"));
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(location1));
+    }
 }
