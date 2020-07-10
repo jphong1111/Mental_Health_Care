343e207d2e2dfa50f3b2e053caf205de360d43e4
Jungpyo Hong <jzh0169@auburn.edu>
2020-07-09 21:23:37 -0500

    make button color and make map

--- app/src/main/java/com/example/mental_health_care/Menu4.java
+++ app/src/main/java/com/example/mental_health_care/Menu4.java
@@ -1,45 +1,121 @@
-/*
 package com.example.mental_health_care;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+
 import com.google.android.gms.maps.model.MarkerOptions;
 
-//resource center about location of mental health care
 public class Menu4 extends FragmentActivity implements OnMapReadyCallback {
 
-    GoogleMap mMap;
-    SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void centreMapOnLocation(Location location, String title){
+
+        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,12));
+
+    }
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centreMapOnLocation(lastKnownLocation,"Your Location");
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu4);
-
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapAPI);
-
         mapFragment.getMapAsync(this);
     }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         mMap = googleMap;
-        LatLng location1 = new LatLng(19,12);
-        mMap.addMarker(new MarkerOptions().position(location1).title("test1"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location1));
-    }
 
+        LatLng center1 = new LatLng(32.620892, -85.405405);
+        googleMap.addMarker(new MarkerOptions().position(center1).title("East Alabama Mental Health"));
+
+        LatLng center2 = new LatLng(32.627221, -85.461050);
+        googleMap.addMarker(new MarkerOptions().position(center2).title("Alabama Center for Trauma Therapy"));
+
+        Intent intent = getIntent();
+
+        if (intent.getIntExtra("Place Number",0) == 0 ){
+
+            // Zoom into users location
+            locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    centreMapOnLocation(location,"Your Location");
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            };
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centreMapOnLocation(lastKnownLocation,"Your Location");
+            } else {
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+        }
+
+
+    }
 
 
 }
-*/
